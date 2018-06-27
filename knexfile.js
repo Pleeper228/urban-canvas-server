f9d3e8cc3241a294c3f87f817430ad0325fa266f
missmeow71 <sensuousdesigner@gmail.com>
2018-06-27 14:11:10 -0600

    update knexfile for production

--- knexfile.js
+++ knexfile.js
@@ -9,11 +9,6 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    }
+    connection: `${process.env.DATABASE_URL}?ssl=true`
   }
-
 };
