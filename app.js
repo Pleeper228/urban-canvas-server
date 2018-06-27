a1b7f4adf14d613c5e72630700dcd5c3b50f9241
missmeow71 <sensuousdesigner@gmail.com>
2018-06-27 15:09:28 -0600

    update cors url

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://localhost:3000',
+  origin: 'http://localhost:3000',
   optionsSuccessStatus: 200
 }))
 
