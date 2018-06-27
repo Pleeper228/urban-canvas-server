208cd35f000158b3fe7cb431e951c7c4af045bf9
missmeow71 <sensuousdesigner@gmail.com>
2018-06-27 14:48:34 -0600

    update knex.js with env variable

--- db/knex.js
+++ db/knex.js
@@ -1,4 +1,4 @@
-const environment = process.env.NODE.ENV || 'development';
+const environment = process.env.NODE_ENV || 'development';
 const config = require('../knexfile');
 const environmentConfig = config[environment];
 const knex = require('knex');
