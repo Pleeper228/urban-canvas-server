412bdd764213dc18dd80d981bbc4c6f8a91af33e
Brevon Good <brevonbgood@comcast.net>
2018-07-01 16:48:58 -0600

    update art card validation to allow -180 to 180 degrees

--- api/art_card.js
+++ api/art_card.js
@@ -7,9 +7,9 @@ const queries = require('../db/queries');
 function validArtCard(artCard) {
     const hasImgUrl = typeof artCard.imgUrl == 'string' && artCard.imgUrl.trim() != ""
     const hasDescription = typeof artCard.description == 'string' && artCard.description.trim() != ""
-    const hasLocation = typeof artCard.location == 'string' && artCard.location.trim() != "" 
-    const hasLat = typeof artCard.lat == 'number' && artCard.lat >= -90 && artCard.lat <= 90
-    const hasLng = typeof artCard.lng == 'number' && artCard.lng >= -90 && artCard.lng <= 90
+    const hasLocation = typeof artCard.location == 'string' && artCard.location.trim() != ""
+    const hasLat = typeof artCard.lat == 'number' && artCard.lat >= -180 && artCard.lat <= 180
+    const hasLng = typeof artCard.lng == 'number' && artCard.lng >= -180 && artCard.lng <= 180
 
     return hasImgUrl && hasDescription && hasLocation && hasLat && hasLng
 }
@@ -32,7 +32,7 @@ router.post('/', (req, res, next) => {
     if (validArtCard(req.body)) {
      queries.create(req.body).then(art_cards => {
          res.json(art_cards[0])
-     })   
+     })
     } else {
         next(new Error('invalid art card'))
     }
